3ec87f97aa96ee146bac3b9918f394ceabf4acff
hanyoseob <hanyoseob@kaist.ac.kr>
2020-04-18 10:59:24 -0600

    update init

---
+++ main.py
@@ -0,0 +1,41 @@
+
+## 라이브러리 추가하기
+import argparse
+
+from train import *
+
+## Parser 생성하기
+parser = argparse.ArgumentParser(description="Regression Tasks such as inpainting, denoising, and super_resolution",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--mode", default="train", choices=["train", "test"], type=str, dest="mode")
+parser.add_argument("--train_continue", default="on", choices=["on", "off"], type=str, dest="train_continue")
+
+parser.add_argument("--lr", default=2e-4, type=float, dest="lr")
+parser.add_argument("--batch_size", default=128, type=int, dest="batch_size")
+parser.add_argument("--num_epoch", default=5, type=int, dest="num_epoch")
+
+parser.add_argument("--data_dir", default="./../datasets/img_align_celeba", type=str, dest="data_dir")
+parser.add_argument("--ckpt_dir", default="./checkpoint/DCGAN", type=str, dest="ckpt_dir")
+parser.add_argument("--log_dir", default="./log/DCGAN", type=str, dest="log_dir")
+parser.add_argument("--result_dir", default="./result/DCGAN", type=str, dest="result_dir")
+
+parser.add_argument("--task", default="GAN", choices=["inpainting", "denoising", "super_resolution", 'GAN'], type=str, dest="task")
+parser.add_argument('--opts', nargs='+', default=['bilinear', 4.0, 0], dest='opts')
+
+parser.add_argument("--ny", default=64, type=int, dest="ny")
+parser.add_argument("--nx", default=64, type=int, dest="nx")
+parser.add_argument("--nch", default=3, type=int, dest="nch")
+parser.add_argument("--nker", default=128, type=int, dest="nker")
+
+parser.add_argument("--network", default="DCGAN", choices=["unet", "hourglass", "resnet", "srresnet", "DCGAN"], type=str, dest="network")
+parser.add_argument("--learning_type", default="plain", choices=["plain", "residual"], type=str, dest="learning_type")
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+
+    if args.mode == "train":
+        train(args)
+    elif args.mode == "test":
+        test(args)
