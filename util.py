d97fa6224f6e6da0e38c194e71936e9a16a1aae3
hanyoseob <hanyoseob@kaist.ac.kr>
2020-04-19 23:03:12 -0600

    update init

--- util.py
+++ util.py
@@ -9,21 +9,33 @@ import torch.nn as nn
 
 ## 네트워크 grad 설정하기
 def set_requires_grad(nets, requires_grad=False):
+    """Set requies_grad=Fasle for all the networks to avoid unnecessary computations
+    Parameters:
+        nets (network list)   -- a list of networks
+        requires_grad (bool)  -- whether the networks require gradients or not
+    """
     if not isinstance(nets, list):
         nets = [nets]
-
     for net in nets:
         if net is not None:
             for param in net.parameters():
                 param.requires_grad = requires_grad
 
 
-
 ## 네트워크 weights 초기화 하기
 def init_weights(net, init_type='normal', init_gain=0.02):
-    def init_func(m):
-        classname = m.__class__.__name__
+    """Initialize network weights.
 
+    Parameters:
+        net (network)   -- network to be initialized
+        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+        init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
+
+    We use 'normal' in the original pix2pix and CycleGAN paper. But xavier and kaiming might
+    work better for some applications. Feel free to try yourself.
+    """
+    def init_func(m):  # define the initialization function
+        classname = m.__class__.__name__
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if init_type == 'normal':
                 nn.init.normal_(m.weight.data, 0.0, init_gain)
@@ -33,15 +45,17 @@ def init_weights(net, init_type='normal', init_gain=0.02):
                 nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
             elif init_type == 'orthogonal':
                 nn.init.orthogonal_(m.weight.data, gain=init_gain)
-
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
             if hasattr(m, 'bias') and m.bias is not None:
                 nn.init.constant_(m.bias.data, 0.0)
-
-        elif classname.find('BatchNorm2d') != -1:
+        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
             nn.init.normal_(m.weight.data, 1.0, init_gain)
             nn.init.constant_(m.bias.data, 0.0)
 
-    net.apply(init_func)
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)  # apply the initialization function <init_func>
+
 
 
 
